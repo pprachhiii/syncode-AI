fb7d6dbee86a3d7f360e74170105acaf7bf5c627
Prachi Yadav <prachiiyadav2409@gmail.com>
2025-11-27 20:37:54 +0530

    fix(frontend): defined motion import

--- Syncode/frontend/src/pages/Home.jsx
+++ Syncode/frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { Button } from "@/components/ui/button";
+import { motion } from "framer-motion";
 
 const Home = () => {
   const securityFeatures = [
